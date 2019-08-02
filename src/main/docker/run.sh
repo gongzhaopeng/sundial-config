69d8cc5653a2b240bf2365ae45188e05c330504a
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-08-02 14:04:37 +0800

    Initializing...

---
+++ src/main/docker/run.sh
@@ -0,0 +1,4 @@
+echo "********************************************************"
+echo "Starting Configuration Service"
+echo "********************************************************"
+java -jar /usr/local/configserver/@project.build.finalName@.jar
